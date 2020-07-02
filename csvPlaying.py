4567a2830e23775f366863d524b6703944f11db5
Jordan Young <jordanby@uci.edu>
2020-07-02 15:46:09 -0700

    adding working with columns

--- csvPlaying.py
+++ csvPlaying.py
@@ -2,4 +2,11 @@
 import pandas as pd
 
 df = pd.read_csv("Sacramentorealestatetransactions.csv")
+
+'''
+print(df.head())
+print(df.describe())
+'''
+
+df['expensive'] = df['price'] > 150000
 print(df.head())
